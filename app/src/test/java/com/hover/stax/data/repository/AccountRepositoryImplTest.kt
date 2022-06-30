a1b58b50bae95870eeaaf286bf4d643027345585
Alex Kombo <alexkombo@gmail.com>
2022-06-30 11:44:21 +0300

    adds kotlin test and truth libraries

--- app/src/test/java/com/hover/stax/data/repository/AccountRepositoryImplTest.kt
+++ app/src/test/java/com/hover/stax/data/repository/AccountRepositoryImplTest.kt
@@ -1,51 +1,117 @@
 package com.hover.stax.data.repository
 
+import com.google.common.truth.Truth.assertThat
+import com.hover.stax.ApplicationInstance
 import com.hover.stax.actions.ActionRepo
+import com.hover.stax.channels.Channel
 import com.hover.stax.channels.ChannelRepo
 import com.hover.stax.data.local.accounts.AccountRepo
 import com.hover.stax.data.model.Account
 import com.hover.stax.domain.repository.AccountRepository
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.mockk
+import com.hover.stax.notifications.PushNotificationTopicsInterface
+import io.mockk.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.test.KoinTest
 
 
 @OptIn(ExperimentalCoroutinesApi::class)
-internal class AccountRepositoryImplTest {
+internal class AccountRepositoryImplTest: KoinTest {
 
     private lateinit var accountRepository: AccountRepository
 
+    private lateinit var accountRepo: AccountRepo
+    private lateinit var channelRepo: ChannelRepo
+    private lateinit var actionRepo: ActionRepo
+
     private val accounts = listOf(
-        Account("Stanbic", "#fefefe"),
-        Account("Safaricom", "#f2f2f2"),
-        Account("Airtel", "#f3f3f3")
+        Account("Stanbic", "#fefefe").apply { id = 1 },
+        Account("Safaricom", "#f2f2f2").apply { id = 2 },
+        Account("Airtel", "#f3f3f3").apply { id = 3 }
+    )
+
+    private val channels = listOf(
+        Channel(651234, "KCB Bank").apply {
+            logoUrl = "logo.jpg"
+            accountNo = "12345676"
+            countryAlpha2 = "KE"
+            primaryColorHex = "#2A561E"
+            secondaryColorHex = "#E2E2E2"
+            defaultAccount = false
+        },
+        Channel(123478, "Telkom").apply {
+            logoUrl = "logo.jpg"
+            accountNo = "087776313"
+            countryAlpha2 = "KE"
+            primaryColorHex = "#3AB4H7"
+            secondaryColorHex = "#EEEEEE"
+            defaultAccount = false
+        }
     )
 
     @Before
     fun setUp() {
-        val accountRepo = mockk<AccountRepo>()
-        val channelRepo = mockk<ChannelRepo>()
-        val actionRepo = mockk<ActionRepo>()
+        accountRepo = mockk()
+        channelRepo = mockk()
+        actionRepo = mockk()
+
         accountRepository = AccountRepositoryImpl(accountRepo, channelRepo, actionRepo, StandardTestDispatcher())
     }
 
-
     @Test
     fun `fetch accounts returns list of accounts`() = runTest {
-        coEvery { accountRepository.fetchAccounts() } returns flowOf(accounts)
+        coEvery { accountRepo.getAccounts() } returns flowOf(accounts)
 
         val accountList = accountRepository.fetchAccounts()
 
-        coVerify { accountRepository.fetchAccounts() }
+        coEvery { accountRepo.getAccounts() }
 
-        assert(accountList.count() > 0)
+        assertThat(accountList.count()).isGreaterThan(0)
+    }
+
+    @Test
+    fun `fetch accounts returns empty when no accounts are set`() = runTest {
+        coEvery { accountRepo.getAccounts() } returns emptyFlow()
+
+        val accountList = accountRepository.fetchAccounts()
+
+        coEvery { accountRepo.getAccounts() }
+
+        assertThat(accountList.count()).isEqualTo(0)
+    }
+
+    @Test
+    fun `create accounts returns ids of created accounts`() = runTest(StandardTestDispatcher()) {
+        startKoin {
+            androidContext(ApplicationInstance())
+        }
+
+        val push = mockk<PushNotificationTopicsInterface>()
+
+        coEvery { accountRepo.getDefaultAccountAsync() } returns accounts.first()
+        every { actionRepo.getActions(channelId = any(), type = any()) } returns emptyList()
+        every { channelRepo.update(channels = any()) } just runs
+        coEvery { accountRepo.insert(accounts = any()) } returns accounts.map { it.id.toLong() }
+
+        every { push.joinChannelGroup(any(), any()) } just runs
+
+        val accountIds = accountRepository.createAccounts(channels)
+
+        coVerify { accountRepo.getDefaultAccountAsync() }
+        verify { actionRepo.getActions(channelId = any(), type = any()) }
+        verify { channelRepo.update(channels = any()) }
+        verify { push.joinChannelGroup(any(), any()) }
+        coVerify { accountRepo.insert(accounts = any()) }
+
+        assertThat(accountIds).contains(accounts.map { it.id.toLong() })
     }
 
 }
